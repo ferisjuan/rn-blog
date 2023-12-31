207ac8dedade1b6ee11a2fa764e96e2b093e9ffa
Juan Feris <feris.juan@gmail.com>
2023-12-31 08:52:35 -0500

    delete post

--- scr/context/BlogContext.js
+++ scr/context/BlogContext.js
@@ -7,19 +7,23 @@ const blogReducer = (state, action) => {
       return [
         ...state,
         {
+          id: Math.floor(Math.random() * 99999),
           title: `Blog Post #${state.length + 1}`,
         }
       ];
+    case "delete_blog_post":
+      return state.filter((post) => post.id !== action.payload);
     default:
       return state;
   }
 }
 
 const addBlogPost = (dispatch) => () => dispatch({ type: "add_blog_post" })
+const deletePost = (dispatch) => (id) => dispatch({ type: "delete_blog_post", payload: id })
 
 export const { Context, Provider } = CreateDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deletePost },
   []
 )
 
--- scr/screens/IndexScreen.js
+++ scr/screens/IndexScreen.js
@@ -1,23 +1,25 @@
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 import { useBlogContext } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { state: blogPosts, addBlogPost } = useBlogContext();
+  const { state: blogPosts, addBlogPost, deletePost } = useBlogContext();
 
   return (
     <View>
       <Button onPress={addBlogPost} title="Add Post" />
       <FlatList
         data={blogPosts}
-        keyExtractor={(blogPost) => blogPost.title}
+        keyExtractor={(blogPost) => blogPost.id}
         renderItem={({ item }) => (
           <View style={styles.row}>
             <Text style={styles.title}>
               {item.title}
             </Text>
-            <Feather name="trash" style={styles.icon} />
+            <TouchableOpacity onPress={() => deletePost(item.id)}>
+              <Feather name="trash" style={styles.icon} />
+            </TouchableOpacity>
           </View>
         )}
       />
